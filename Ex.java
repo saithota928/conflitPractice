134620bc299f51bffe1387028e9804b949bc755c
susmitha <susmitha.t9642@gmail.com>
2024-08-06 17:42:59 +0530

    commit

--- Ex.java
+++ Ex.java
@@ -1,6 +1,6 @@
 public class Ex{
     public static void main(String[] args){
-        List<String> list = new ArrayList<>();
+        Map<Integer,String> map = new HashMap<>();
         System.out.println("Hello world");
     }
 }
