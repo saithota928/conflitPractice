8160ae92fda98229539ab965319a9f3979bf6dc9
susmitha <susmitha.t9642@gmail.com>
2024-08-06 17:44:42 +0530

    commit

--- Ex.java
+++ Ex.java
@@ -1,6 +1,7 @@
 public class Ex{
     public static void main(String[] args){
-        List<String> list = new ArrayList<>();
+       
+        String = "hi";
         System.out.println("Hello world");
     }
 }
